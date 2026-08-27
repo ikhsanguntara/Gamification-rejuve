07a140c6592328fe4b7e7b6102dbb01c5d7d7620
ikhsan <ikhsan.guntara98@gmail.com>
2026-08-27 13:10:58 +0700

    chore(deploy): configure static SPA generation for hosting and ignore firebase cache

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
+  ssr: false,
+
+  nitro: {
+    prerender: {
+      crawlLinks: false,
+      routes: ['/']
+    }
+  },
 
   modules: [
     '@nuxtjs/tailwindcss',
